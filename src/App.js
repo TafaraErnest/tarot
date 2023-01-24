d27911610f9e333d8393288f3d0f34ca587dfcf5
TafaraErnest <tafara618@gmail.com>
2023-01-24 14:34:21 +0800

    Fix bugs, increase responsiveness

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 import "./App.css";
 import Cards from "./components/Cards";
-import Flip from "./components/Flip";
 
 export default function App() {
   const [cards, setCards] = useState([]);
+  const [spinCard, setSpinCard] = useState(false);
 
   const generate = () => {
     const arr = [];
@@ -13,7 +13,8 @@ export default function App() {
       arr.push({
         id: nanoid(),
         number: Math.ceil(Math.random() * 78),
-        rotate: false,
+        rotate: Math.random() > 0.5 ? true : false,
+        flipped: false,
       });
     }
     return arr;
@@ -26,23 +27,27 @@ export default function App() {
   const flipCard = (id) => {
     setCards(
       cards.map((card) => {
-        if (card.id === id && !card.rotate) {
-          return { ...card, rotate: true };
-        } else if (card.id === id && card.rotate) {
-          return { ...card, rotate: false };
+        if (card.id === id && !card.flipped) {
+          return { ...card, flipped: true };
+        } else if (card.id === id && card.flipped) {
+          return { ...card, flipped: false };
         } else return card;
       })
     );
   };
 
-  const resetAll = () => {
-    setCards(generate());
-  };
-
   return (
-    <main>
-      <Cards cards={cards} flipCard={flipCard} />
-      <Flip resetAll={resetAll} />
+    <main className="page">
+      <h2 className="title">Yuan Tarot</h2>
+      <Cards cards={cards} flipCard={flipCard} spinCard={spinCard} />
+      <button
+        onClick={() => {
+          setCards(generate());
+          setSpinCard(true);
+        }}
+      >
+        Shuffle Cards
+      </button>
     </main>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,18 +1,23 @@
-export default function Cards({ cards, flipCard }) {
+// flipCard
+export default function Cards({ cards, flipCard, spinCard }) {
   return (
     <div className="cards">
       {cards.map((card) => {
         return (
-          <div className="scene" key={card.id}>
+          <div
+            className="scene"
+            key={card.id}
+            id={spinCard && card.rotate ? "spinCard" : "spinAnti"}
+          >
             <div
               className="card"
-              id={card.rotate ? "is-flipped" : ""}
+              id={card.flipped ? "is-flipped" : ""}
               onClick={() => flipCard(card.id)}
             >
               <div className="face front">
                 <img src="./images/back.png" alt="" />
               </div>
-              <div className="face back">
+              <div className="face back" id={card.rotate ? "rotate" : ""}>
                 <img src={`./images/img${card.number}.png`} alt="..." />
               </div>
             </div>
